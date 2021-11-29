3945e1ca8e83a172f3f5cb2148f17b42e7bca437
s20184 <s20184@pjwstk.edu.pl>
2021-11-29 20:44:21 +0100

    Tutorial 8 - all routes finished

--- Tutorial 8/tut8.js
+++ Tutorial 8/tut8.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
 const app = express();
 const port = 8080;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/hello', (req,res) => {
     res.send('Hello world!');
@@ -20,9 +20,22 @@ app.get('/form', (req,res) => {
 app.post('/formdata', (req,res) => {
     var username = req.body.username;
     var email = req.body.email;
+    var age = req.body.age;
     res.render('formdata', {
         username: username,
-        email: email
+        email: email,
+        age: age
+    });
+})
+
+app.post('/jsondata', (req,res) => {
+    var name = req.body.name;
+    var species = req.body.species;
+    var toy = req.body.toy;
+    res.render('jsondata', {
+        name: name,
+        species: species,
+        toy: toy
     });
 })
 
